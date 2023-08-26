4a159a2714b71959506919aac312b18af65209b2
retrx0 <61639630+retrx0@users.noreply.github.com>
2023-08-26 22:58:10 +0200

    kafka integration

--- apps/bookingServer/src/main/java/com/nsi/bs/config/kafka/KafkaConsumerConfig.java
+++ apps/bookingServer/src/main/java/com/nsi/bs/config/kafka/KafkaConsumerConfig.java
@@ -1,20 +1,19 @@
 package com.nsi.bs.config.kafka;
 
-import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
 
-import static com.nsi.common.constants.KafkaConfigConstants.*;
+import static com.nsi.common.constants.KafkaConfigConstants.KAFKA_FSBS_BOOTSTRAP_ADDRESS;
+import static com.nsi.common.constants.KafkaConfigConstants.KAFKA_GROUP_ID;
 
 @Configuration
 @EnableKafka
--- libs/libcommon/src/main/java/com/nsi/common/constants/KafkaConfigConstants.java
+++ libs/libcommon/src/main/java/com/nsi/common/constants/KafkaConfigConstants.java
@@ -3,7 +3,9 @@ package com.nsi.common.constants;
 public class KafkaConfigConstants {
 
     public static final String KAFKA_FSBS_BOOTSTRAP_ADDRESS = "localhost:9092";
+    public static final String KAFKA_BOOKING_BOOTSTRAP_ADDRESS = "localhost:9092";
     public static final String KAFKA_FSBS_TOPIC = "kafka_fsbs";
     public static final String KAFKA_BOOKING_TOPIC = "kafka_booking";
     public static final String KAFKA_GROUP_ID = "group1";
+    public static final String KAFKA_BOOKING_GROUP_ID = "group2";
 }
