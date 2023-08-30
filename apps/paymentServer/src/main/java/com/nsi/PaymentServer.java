6993243e25f92cf0af6e8741d3a1feb617e6d9e7
retrx0 <61639630+retrx0@users.noreply.github.com>
2023-08-30 11:19:25 +0200

    project refactoring

---
+++ apps/paymentServer/src/main/java/com/nsi/PaymentServer.java
@@ -0,0 +1,11 @@
+package com.nsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentServer {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentServer.class, args);
+    }
+}
---
+++ libs/libpayment/src/main/java/com/nsi/model/Payment.java
@@ -0,0 +1,4 @@
+package com.nsi.model;
+
+public class Payment {
+}
